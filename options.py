85c1f28e86caf6b8a9ca1901da78a1fe017a3855
Sudipan Saha <sudipan.saha@gmail.com>
2020-09-20 13:40:04 +0200

    updated for multiple CD

--- options.py
+++ options.py
@@ -27,6 +27,13 @@ class optionsDCVA():
         parser.add_argument('--objectMinSize', type=int, default=128, help='minimum size for objects in pixel')
         parser.add_argument('--topPercentSaturationOfImageOk', type=bool, default=True, help='bool indicating if images are preprocessed to saturate high values')
         parser.add_argument('--topPercentToSaturate', type=float, default=1, help='percentage to saturate')
+        ##Remaining options are multiple CD related. They can be ignored if only binary CD is performed.
+        parser.add_argument('--multipleCDBool', type=bool, default=False, help='(True/False)whether multiple CD is needed. If false only binary CD will be performed')
+        parser.add_argument('--changeVectorBinarizationStrategy', type=str, default='otsuThreshold', help='otsuThreshold/zeroThreshold, for multiple CD - strategy for binarizing deep change vector')
+        parser.add_argument('--clusteringStrategy', type=str, default='hierarchical', help='hierarchical/kmodes, for multiple CD - strategy for clustering binarized deep change vector')
+        parser.add_argument('--hierarchicalDistanceStrategy', type=str, default='hamming', help='hamming/correlation, for multiple CD hierarchical clusteting - method for calculating distance between binarized deep change vectors')
+        parser.add_argument('--clusterNumber', type=int, default=2, help='for multiple CD - cluster number')
+        
         
         self.initialized = True
         return parser
